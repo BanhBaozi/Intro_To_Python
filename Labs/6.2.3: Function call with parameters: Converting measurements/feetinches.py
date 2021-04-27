e42d462c98d1bf2deb9d956ecf83f5f517f4b31b
Jenny Nguyen <40232531+BanhBaozi@users.noreply.github.com>
2021-04-26 23:08:53 -0700

    Create feetinches.py

---
+++ Labs/6.2.3: Function call with parameters: Converting measurements/feetinches.py
@@ -0,0 +1,8 @@
+
+def print_total_inches(num_feet,num_inches):
+
+     print("Total inches: "+ str(12 * num_feet + num_inches))
+     
+feet = int(input())
+inches = int(input())
+print_total_inches(feet, inches)
